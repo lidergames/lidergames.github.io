3d77365913e8e0f4c35a1448c9960e0268238160
lidergames <44867278+lidergames@users.noreply.github.com>
2018-11-08 21:07:51 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var game;
 var GameInitialize = function() {    
 game = new Phaser.Game(576, 288, Phaser.CANVAS);
-    game.state.add("PlayGame", playGame, true);
-    game.state.add("SlashScreen", slashScreen, false);
+    game.state.add("PlayGame", playGame, false);
+    game.state.add("SlashScreen", slashScreen, true);
 }
