fe93d49872b9323dd8236706aaad1e08b7266fd7
lidergames <44867278+lidergames@users.noreply.github.com>
2018-12-07 13:04:26 +0300

    d

--- site/games/game_alex/js/screen/dreamlosdk.js
+++ site/games/game_alex/js/screen/dreamlosdk.js
@@ -1,4 +1,15 @@
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
 
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
 var dreamloSDK = {
 	init : function() {
 		this.privateKey = 'jOkFyRr48k2t8jN8Ao0wKguHC59lL42kmRxvOUEbNdng';
@@ -6,21 +17,26 @@ var dreamloSDK = {
 		this.XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
 		this.xhr = new this.XHR();
+		this.client = new HttpClient();
 	},
 	getLB: function() {
 		var leaders;
-		if ( this.xhr ) {
-			this.xhr.open( "GET", "dreamlo.com/lb/5bf48cceb6397e00e07d1692/json", false );
-			this.xhr.onload = function() {
-					leaders = JSON.parse(this.responseText).dreamlo.leaderboard.entry;
-			}
-			this.xhr.onerror = function() {
-					alert( "Не удалось сделать запрос" );
-			}
-			this.xhr.send();
-		} else {
-			alert( "Извините но ваш браузер не поддерживает Кроссдоменные запросы." );
-		}
+		this.client.get("dreamlo.com/lb/5bf48cceb6397e00e07d1692/json",function(response) {
+				leaders = JSON.parse(response).dreamlo.leaderboard.entry;
+			});
+		//if ( this.xhr ) {
+		//	
+		//	this.xhr.open( "GET", "dreamlo.com/lb/5bf48cceb6397e00e07d1692/json", false );
+		//	this.xhr.onload = function() {
+		//			leaders = JSON.parse(this.responseText).dreamlo.leaderboard.entry;
+		//	}
+		//	this.xhr.onerror = function() {
+		//			alert( "Не удалось сделать запрос" );
+		//	}
+		//	this.xhr.send();
+		//} else {
+		//	alert( "Извините но ваш браузер не поддерживает Кроссдоменные запросы." );
+		//}
 		return leaders;
 	},
 	pushScore: function(name,countObjects,timeGame) {
